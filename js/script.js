3ca94a5c85c5d088639e42ae6d6085e2c67b8c3a
matteovecchio04 <matteovex@gmail.com>
2025-10-28 12:17:45 +0100

    add catch

--- js/script.js
+++ js/script.js
@@ -14,10 +14,14 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/")
               <p class="card-text">${polar.title}</p>
             </div>
           </div>
-        `).join('')}
+        `).join(``)}
       </div>`;
     }) 
     // i had to put the resetContent inside the .then because otherwise, content was not defined outside the .then
+    .catch(err => {
+        board.innerHTML = "<p>Error connecting with server</p>"
+        console.log(err);
+    })
 
 
 
