75c3d0edbdfbf92f4840ede215df6874d52df6f8
matteovecchio04 <matteovex@gmail.com>
2025-10-28 12:08:56 +0100

    add dynamic card creation with axios

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
 axios.get("https://lanciweb.github.io/demo/api/pictures/")
-.then( resp => {
-    console.log(resp.data);
-    const content = resp.data
-})
+    .then(resp => {
+        console.log(resp.data);
+        const content = resp.data
+
+        const boardEl = document.getElementById("board")
+
+        boardEl.innerHTML = `
+      <div class="row gap-4 justify-content-center">
+        ${content.map(polar => `
+          <div class="card p-3 col-sm-12 col-md-6 col-lg-4" style="width: 18rem;">
+            <img src="${polar.url}" class="card-img-top">
+            <div class="card-body">
+              <p class="card-text">${polar.title}</p>
+            </div>
+          </div>
+        `).join('')}
+      </div>`;
+    }) 
+    // i had to put the resetContent inside the .then because otherwise, content was not defined outside the .then
+
+
 
